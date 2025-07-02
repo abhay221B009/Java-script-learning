db229040d65ea88cfa51324230d373c10c312832
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-07-02 20:45:38 +0530

    codes while learning javascript

---
+++ conditional.js
@@ -0,0 +1,14 @@
+//conditional statements
+
+let age = 41;
+if (age >= 18) {
+  console.log("Eligible to vote");
+}
+if (age < 18) {
+  console.log("Underage");
+}
+if (age < 40) {
+  console.log("Sarkar apni hi hai");
+} else {
+  console.log("mat do vote");
+}
