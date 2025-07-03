fba0bd21bb20434fb6b5f06c468d3144d154a1f2
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-07-03 14:10:55 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,15 @@
+//For loop
+
+for (let i = 0; i < 5; i++) {
+  console.log("Hello World");
+}
+
+console.log(
+  "=====================sum of first n natural numbers=================="
+);
+//sum of first n natural numbers
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+  sum += i;
+}
+console.log("Sum of first 100 natural numbers is: " + sum);
---
+++ practice1.js
@@ -0,0 +1,6 @@
+let num = prompt("Enter a number:");
+if (num % 5 === 0) {
+  console.log("multiple of 5");
+} else {
+  console.log("not a multiple of 5");
+}
