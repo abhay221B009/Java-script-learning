3dbf9170556ad30bc1115e8b96368e65bbcccc17
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-07-25 23:35:15 +0530

    latest

--- Advance/DocumentObjectModel.js
+++ Advance/DocumentObjectModel.js
@@ -2,6 +2,8 @@
 // // console.log("hello user");
 // // alert("this is abhay ");
 
+// const { createElement } = require("react");
+
 // console.log(window);
 // window.console.log("hello2");
 
@@ -33,11 +35,11 @@
 //document.querySelectorAll("myId/myClass/tag")
 //this returns a nodelist
 
-console.log("abhay");
+// console.log("abhay");
 // window.log("hello");
 // window.alert("welcome to javascript learning    ");
 
-alert("abhay");
+// alert("abhay");
 
 // 15 july
 
@@ -53,8 +55,42 @@ alert("abhay");
 
 // console.log(div.setAttribute("box", "rectangle"));
 
+// let para = document.querySelector("p");
+// // para.style.color = "green";
+// // para.style.fontSize = "100px";
+// // para.style.borderColor = "magenta";
+// // para.style.backgroundColor = "white";
+// para.innerText = "hello world";
+// para.style.visibility = "hidden";
+//16 july dom manipulation
+
+//---INSERT_ELEMENTS---
+let newBtn = document.createElement("button");
+newBtn.innerText = "click me";
+console.log(newBtn);
+//node.append(el)
+// document.body.append(newBtn);
+// node.prepend(el);
+document.body.prepend(newBtn);
+//node.before(el)
+//document.body.before(newBtn);
+//node.after(el)
+//document.body.after(newBtn);
+//node.replaceWith(el)
+//document.body.replaceWith(newBtn);
+//node.remove()
+//newBtn.remove();
+
+let newHeading = document.createElement("h1");
+newHeading.innerText = "This is a new heading";
+document.body.prepend(newHeading);
+
+//DELETE ELEMENTS
 let para = document.querySelector("p");
-para.style.color = "green";
-para.style.fontSize = "100px";
-para.style.borderColor = "magenta";
-para.style.backgroundColor = "white";
+para.remove();
+//para.removeChild(para.firstChild); //removes first child of para
+//para.removeChild(para.lastChild); //removes last child of para
+//para.removeChild(para.childNodes[0]); //removes first child of para
+//para.removeChild(para.childNodes[para.childNodes.length - 1]); //removes last child of para
+//para.removeChild(para.childNodes[1]); //removes second child of para
+//para.removeChild(para.childNodes[para.childNodes.length - 2]); //rem
