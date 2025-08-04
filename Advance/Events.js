e3f03004747f5885e3ab1be72232b4796bc3b5bd
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-08-05 01:09:29 +0530

    events

---
+++ Advance/Events.js
@@ -0,0 +1,9 @@
+let btn1 = document.querySelector("#btn1");
+btn1.onclick = function () {
+  console.log("button was clicked");
+  alert("button was clicked");
+  let a = 30;
+  console.log(a);
+  a++;
+  console.log(a);
+};
