f3db6c033d990c25926302d0fa39793841862e5d
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-08-05 01:29:26 +0530

    created a box and added a event

--- Advance/Events.js
+++ Advance/Events.js
@@ -1,5 +1,10 @@
 let btn1 = document.querySelector("#btn1");
-btn1.onclick = function () {
+btn1.onclick = function (e) {
+  console.log(e);
+  console.log(e.type);
+  console.log(e.target);
+  console.log(e.clientX, e.clientY); // Mouse position relative to the viewport
+
   console.log("button was clicked");
   alert("button was clicked");
   let a = 30;
@@ -16,3 +21,5 @@ box.onmouseout = function () {
   console.log("mouse left the box");
   box.style.backgroundColor = "blue"; // Revert to original
 };
+
+//EVENT OBJECT -> HAVE THE INFORMATION ABOUT THE EVENT.
