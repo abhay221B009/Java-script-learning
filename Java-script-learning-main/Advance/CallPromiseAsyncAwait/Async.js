d5f7b450a9f7f6df0ec040f362ebe4799768d2f8
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-08-27 19:27:45 +0530

    callback promise , async

---
+++ Java-script-learning-main/Advance/CallPromiseAsyncAwait/Async.js
@@ -0,0 +1,6 @@
+//async>>promise chains>>callback hell.
+//synchronous means code runs in sequence of instructions given in the program.
+//each instruction waits for the previous instruction to complete before executing.
+
+//asynchronous means code runs independently of the main program flow.
+//each instruction can execute without waiting for the previous instruction to complete.
